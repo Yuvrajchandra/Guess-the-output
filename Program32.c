3b2005447f7421cc9611f351199e4c8c9e2bd755
Yuvrajchandra <53931942+Yuvrajchandra@users.noreply.github.com>
2020-06-01 21:47:37 +0530

    Create Program32.c

---
+++ Program32.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main()
+{
+    int a=6;
+    printf("\n %d", a--);       //output: 6 //a=5
+    printf("\n %d", ++a);       //output: 6 //a=6
+    printf("\n %d", a++);       //output: 6 //a=7
+    
+}
+
