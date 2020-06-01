f18de1f9a6939c88a424fb69151c8d8c2730f760
Yuvrajchandra <53931942+Yuvrajchandra@users.noreply.github.com>
2020-06-01 20:41:43 +0530

    Create Program23.c

---
+++ Program23.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+main()
+{
+    int a=5;
+    a= printf("good") + printf("girl");
+    printf("%d",a);
+
+}
+
+
+//OPTIONS
+// A. 5
+// B. 8
+// C. 9
+// D. COMPILETIME ERROR
+
+//correct option B. 8
+//as printf() returns the no of characters in double quotes
