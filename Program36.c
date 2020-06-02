e7468fae617736c4a52501247b4ac50ae4edb17a
Yuvrajchandra <53931942+Yuvrajchandra@users.noreply.github.com>
2020-06-02 21:49:06 +0530

    Create Program36.c

---
+++ Program36.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main()
+{
+    int x=1;
+    if(x==2)
+    printf("Hii");
+    printf("Hello");
+}
+
+//OPTIONS
+// A. COMPILETIME ERROR
+// B. Hii
+// C. Hello
+
+
+//correct answer- Hello
+// when we don't give parenthesis in if block , one statement after the if block is considered if block
