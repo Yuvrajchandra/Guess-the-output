a4ffce04e2200a573d039c1b7f3c42d6b38e65e9
Yuvrajchandra <53931942+Yuvrajchandra@users.noreply.github.com>
2020-06-02 21:49:24 +0530

    Create Program37.c

---
+++ Program37.c
@@ -0,0 +1,26 @@
+//how many times hi will be printed
+
+#include<stdio.h>
+main()
+{
+    int i=1;
+
+    while(i<10)
+    {
+    printf("hello\n");
+    }
+
+printf("hi");
+}
+
+//OPTIONS
+// A. COMPILETIME ERROR
+// B. 1
+// C. 0
+// D. 9
+
+
+
+
+
+//CORRECT ANSWER- 0 times as while loop be executed infinite times and hi will never be printed
