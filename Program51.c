8a80ac5b82898d6a10db54c4cf63b733b8602390
Yuvrajchandra <53931942+Yuvrajchandra@users.noreply.github.com>
2020-06-03 21:20:24 +0530

    Create Program51.c

---
+++ Program51.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+main()
+{
+    int a,b;
+    a=3+2,b=a,12;
+    printf("%d %d",a,b);
+}
+
+//output-
+//5 5
+
+
+
+
+
+//this is because-
+//first 3+2=5 will be assigned to a
+//then value of a will be assigned to b
+//12 will not have any effect neither it is invalid 
