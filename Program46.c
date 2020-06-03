bb384bfee4df80c0a2f6418f54ab80f1d1c25ac0
Yuvrajchandra <53931942+Yuvrajchandra@users.noreply.github.com>
2020-06-03 21:18:46 +0530

    Create Program46.c

---
+++ Program46.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+    int a;
+    a= printf("hai%dtrish",printf("india"));
+    printf("%d",a);
+}
+
+//output-
+//indiahai5trish9
+
+//printf will be evaluated from right to left and it will return the no. of characters
