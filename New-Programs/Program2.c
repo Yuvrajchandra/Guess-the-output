d642cec4a3d9ccaa9390486b62575ea2e4450868
Yuvraj Chandra <Singhyuvraj179@gmail.com>
2020-06-05 17:27:24 +0530

    Create Program2.c

---
+++ New-Programs/Program2.c
@@ -0,0 +1,19 @@
+int main()
+{
+	char str[10]="Hello";
+	printf("%d,%d\n",strlen(str),sizeof(str));
+	return 0;
+}
+
+
+
+/*
+Output:
+5,10
+
+Explanation:
+strlen gives length of the string that is 5; 
+sizeof gives total number of occupied memory for a variable that is 8;
+since str is a pointer so sizeof(str) may be 2,4 or 8. 
+It depends on the computer architecture.
+*/
