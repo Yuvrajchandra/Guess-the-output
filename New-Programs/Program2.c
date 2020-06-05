14b3d95c8de1df7af96ad4f91557ce5e1be1af7c
Yuvraj Chandra <Singhyuvraj179@gmail.com>
2020-06-05 17:31:59 +0530

    Update Program2.c

--- New-Programs/Program2.c
+++ New-Programs/Program2.c
@@ -13,7 +13,6 @@ Output:
 
 Explanation:
 strlen gives length of the string that is 5; 
-sizeof gives total number of occupied memory for a variable that is 8;
-since str is a pointer so sizeof(str) may be 2,4 or 8. 
-It depends on the computer architecture.
+sizeof gives total number of occupied memory for a variable that is 10;
+Since str is a variable with maximum number of characters 10, so sizeof will be 10.
 */
